b93744b4e806b1907d4f7b7b856cee9314fd694f
Varun Teja <Varun Teja>
2016-09-15 12:28:15 -0400

    file security

--- Encryption/Security.py
+++ Encryption/Security.py
@@ -1,6 +1,6 @@
 '''Copyright (c) 2015 HG,DL,UTA
    Python program runs on local host, uploads, downloads, encrypts local files to google.
-   Please use python 2.7.X, pycrypto 2.6.1 and Google Cloud python module '''
+   Please use python 2.7.X, pycrypto 2.6.1 and Google Cloud python module ''' 
 
 #import statements.
 import argparse
